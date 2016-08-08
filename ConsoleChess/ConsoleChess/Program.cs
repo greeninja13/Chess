56e84ebf18313e700f68300abe4f683649d06d49
Brad <greeninja13@gmail.com>
2016-08-07 21:57:34 -0600

    Started some things, moved the files up a folder to reduce redundancy of the same folder name. Hope nothing broke.

---
+++ ConsoleChess/ConsoleChess/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleChess.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string location = "";
+            FileParser fP = new FileParser();
+
+            if (args.Length > 0 && !string.IsNullOrEmpty(args[0]))
+            {
+                location = args[0];
+                Console.WriteLine("Using File: " +location);
+                fP.ParseFile(location);
+            }
+            else
+            {
+                Console.WriteLine("Empty file location in args");
+            }
+
+        }
+    }
+}
---
+++ ConsoleChess/ConsoleChess/Tools/FileParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleChess.Tools
+{
+    public class FileParser
+    {
+        public string[] ParseFile(string filename)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), filename);
+            StreamReader file = new StreamReader(path);
+
+            return new string[0];
+        }
+    }
+}
